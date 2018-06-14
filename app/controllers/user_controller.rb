01b3fbc4bdff6258d50da63a1991a0e5ce220314
Kaue Matteucci <kauefm@gmail.com>
2018-06-14 15:59:00 -0300

    Carrier Wave implemented sucesfully

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,8 @@ class UserController < ApplicationController
   def index
 
     User.all.each do |user|
+      user.remove_avatar!
+      user.save!
       user.delete
     end
 
@@ -10,7 +12,9 @@ class UserController < ApplicationController
     repos = JSON.parse(response) # "parse das informações em JSON
 
     repos["results"].each do |result| #create a new instance of the model user
+
       user = User.new
+
       user.genre = result["gender"] #select gender
       if user.genre == "male"
         user.genre = "masculino"
@@ -23,9 +27,11 @@ class UserController < ApplicationController
       user.last_name = result["name"]["last"] #select last name
       user.email = result["email"] #select email
       user.picture = result["picture"]["large"] #picture address
-      user.save
+      user.remote_avatar_url = user.picture
+      user.save!
 
     end
       @users = User.all.order(:first_name).page params[:page]
   end
+
 end
